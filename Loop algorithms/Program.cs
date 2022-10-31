0b0e0c2e330ab4e441a1aa96960eee36d521b249
EhtiramShukurov <tu74pw0kj@code.edu.az>
2022-10-31 22:36:52 +0400

    added task scripts

---
+++ Loop algorithms/Program.cs
@@ -0,0 +1,71 @@
+﻿#region Task 1
+//int num = 9, numCopy;
+//int count = 0;
+//int isPower = 0;
+//int smallPower = 1;
+//numCopy = num;
+//while (numCopy != 1)
+//{
+//    if (numCopy % 2 != 0)
+//    {
+//        isPower = 1;
+//    }
+//        numCopy /= 2;
+//        count++;
+//}
+//if( isPower  == 1)
+//{
+//    for (int i = 0; i < count; i++)
+//    {
+//        smallPower *= 2;
+//    }
+//    if (smallPower * 2 - num >= num - smallPower)
+//    {
+//        Console.WriteLine(count);
+//    }
+//    else
+//    {
+//        Console.WriteLine(count + 1);
+//    }
+//}
+//else
+//{
+//    Console.WriteLine(count);
+//}
+#endregion
+#region Task 2
+//int sum = 0;
+//int number = 43864;
+//while (number != 0)
+//{
+//    sum += number % 10;
+//    number /= 10;
+//}
+//Console.WriteLine(sum);
+#endregion
+#region Task 3
+//int number = 567;
+//int count = 0;
+//if (number == 1)
+//{
+//    Console.WriteLine("eded ne sadedir, ne de murekkeb");
+//}
+//else
+//{
+//    for (int i = 1; i <= number; i++)
+//    {
+//        if (number % i == 0)
+//        {
+//            count++;
+//        }
+//    }
+//    if (count == 2)
+//    {
+//        Console.WriteLine("eded sadedir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("eded murekkebdir");
+//    }
+//}
+#endregion
